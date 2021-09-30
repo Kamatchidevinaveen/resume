1e989dd4fbba82730a9ee140914aae266e47601c
kamatchidevinaveen <kamatchidevi97@gmail.com>
2021-09-30 18:59:22 +0530

    Mobile view changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ResumeComponent } from './resume/resume.component';
 import { MaterialModule } from './material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { LayoutModule } from '@angular/cdk/layout';
 @NgModule({
   declarations: [AppComponent, ResumeComponent],
   imports: [
@@ -16,6 +16,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MaterialModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
